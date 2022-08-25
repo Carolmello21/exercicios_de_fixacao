fefb908593e8ccf69a18a206b7fdb8b5bc31851c
Carol Melo <carolimavalente200859@gmail.com>
2022-08-25 18:41:41 -0300

    exercício 3

---
+++ exercicio_3.js
@@ -0,0 +1,10 @@
+// 3 - Faça uma função JS que recebe quatro notas de um aluno, calcule e escreva no console a média final 
+//deste aluno. Considerar que a média é ponderada e que o peso das notas é 2, 3 e 4 e 7. A fórmula para o
+// cálculo da média final é: (n1*2 + n2*3 + n3*4 + n4*7)/16
+
+let n1 = 5.0;
+let n2 = 5.0;
+let n3 = 8.5;
+let n4 = 7.0;
+let media = (n1*2 + n2*3 + n3*4 + n4*7)/16;
+ console.log (`A média final do aluno é: ${media}`);
