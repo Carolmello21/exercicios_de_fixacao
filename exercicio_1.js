1ba50d0d49c8ae0834a24373711725a3442bf9a7
Carol Melo <carolimavalente200859@gmail.com>
2022-08-25 18:16:20 -0300

    exercício

---
+++ exercicio_1.js
@@ -0,0 +1,7 @@
+// 1 - Escreva uma função JS para receber o salário mensal atual de um funcionário. Calcular e retornar o
+// valor do novo salário com 22% de reajuste.
+
+let salarioAtual = 1.300;
+let salarioNovo = (salarioAtual * 1.22);
+
+console.log(`O salário com reajuste é: ${salarioNovo}`)
